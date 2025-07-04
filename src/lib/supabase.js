7ebeb8c5440e2f4f18db0c5a886817b91115fbf2
Adhil Bijukumar <75608493+Adhil-B@users.noreply.github.com>
2025-07-04 13:08:15 +0530

    integrated supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://your-project.supabase.co',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'public-anon-key-here'
+  'https://loxzlhohdidyucrtmsbw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxveHpsaG9oZGlkeXVjcnRtc2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MTM5MTEsImV4cCI6MjA2NzE4OTkxMX0.3jbwnmlQHUaO5KC6WZ98gZkHdnaMUDi8-r-aDeH--QI'
 ); 
